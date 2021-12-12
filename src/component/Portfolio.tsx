fdc550357dfbc4cd426cf741cc7b4e567d5ec57e
kyus <kyus@aty.kr>
2021-12-12 23:06:41 +0900

    implement project thumb

--- src/component/Portfolio.tsx
+++ src/component/Portfolio.tsx
@@ -4,8 +4,11 @@ import {ifiveProject, atyProject} from "../data.json";
 interface listProps {
   name: string;
   src: string;
+  role: string;
+  result: string;
   duration: string;
   detail: string;
+  bg: {src: string; type: string;};
   tag: string[];
 }
 
@@ -21,7 +24,11 @@ function ProjectList({title, list, toggle, openDetail}:{
         {title}
       </div>
       {list.map((p, k) => {
-        return (<div key={k} className={"card-wrapper"} onClick={() => openDetail(p)}>
+        return (<div
+          key={k}
+          className={"card-wrapper"}
+          onMouseEnter={() => toggle(p)}
+          onClick={() => openDetail(p)}>
           <div className={"img-wrapper"}>
             <img src={p.src} alt={p.name}/>
             <div className={"tag-wrapper"}>
@@ -45,35 +52,71 @@ function Popup({data, close}: {data:listProps; close:Dispatch<boolean>}) {
         <div className={"popup-header"}>
           <div>{data.name}</div>
           <div className={"btn-wrapper"}>
-            <AiOutlineClose style={{cursor: "pointer"}} size={50} onClick={() => close(false)} />
+            <AiOutlineClose style={{cursor: "pointer"}} size={30} onClick={() => close(false)} />
           </div>
         </div>
         <div className={"popup-body"}>
-          {data.duration}
-          <pre>{data.detail}</pre>
+          <div className={"info-wrapper"}>
+            <div className={"info-title"}>[개발 기간]</div>
+            <div>{data.duration}</div>
+          </div>
+          <div className={"info-wrapper"}>
+            <div className={"info-title"}>[개발 언어]</div>
+            <div>{data.tag.join(' / ')}</div>
+          </div>
+          <div className={"info-wrapper"}>
+            <div className={"info-title"}>[역할]</div>
+            <div>{data.role}</div>
+          </div>
+          <div className={"info-wrapper"}>
+            <div className={"info-title"}>[성과]</div>
+            <div>{data.result}</div>
+          </div>
+          <div className={"info-wrapper"}>
+            <div className={"info-title"}>[상세]</div>
+            <div>{data.detail.split("\n").map((d, k) => <div key={k}>{d}</div>)}</div>
+          </div>
         </div>
       </div>
     </div>
   )
 }
 
+function ThumbnailBg({bg}:{bg:{src:string; type:string}}) {
+  return (<>
+    {bg.type === 'img' && <img className="bg" src={bg.src} alt={"bg"} />}
+    {bg.type === 'media' && <iframe className={"bg"} src={bg.src} />}
+  </>)
+}
+
 function Thumbnail({thumb}:{thumb:any}) {
   return (
     <div className={"thumb-wrapper"}>
-      thumb layer
+      <div className={"thumb-info"}>
+        <div className={"thumb-title"}>{thumb.name}</div>
+        <div className={"thumb-content"}>{thumb.role} ({thumb.duration})</div>
+        <div className={"thumb-content"}>{thumb.result}</div>
+        <div className={"thumb-tag"}>{thumb.tag.join(" / ")}</div>
+      </div>
+      <div className={"thumb-bg-wrapper"}>
+        <ThumbnailBg bg={thumb.bg} />
+        <div className={"bg-color"} />
+      </div>
     </div>
   )
 }
 
 function Portfolio() {
-  const [thumb, setThumb] = useState({});
+  const [thumb, setThumb] = useState(atyProject[1]);
   const [openPopup, setOpenPopup] = useState(false);
   const [data, setData] = useState(ifiveProject[0]);
   const openDetail = useCallback((data: listProps) => {
     setData(data);
     setOpenPopup(true);
   }, []);
-  const toggle = useCallback((data: listProps) => {}, []);
+  const toggle = useCallback((data: listProps) => {
+    setThumb(data);
+  }, []);
   return (
     <div className={"portfolio-wrapper"}>
       <Thumbnail thumb={thumb} />
